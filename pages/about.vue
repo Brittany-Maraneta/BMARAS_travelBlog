8e25b844d93929b3e034f53a2f97e2a88b116d87
Brittany-Maraneta <Brittany-june@hotmail.com>
2024-04-09 13:16:31 -0600

    bugs

--- pages/about.vue
+++ pages/about.vue
@@ -4,5 +4,5 @@
   <header>
         <img src="https://images.pexels.com/photos/7009459/pexels-photo-7009459.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="h-[700px] w-full rounded-2xl object-cover"></img>
       </header>
-  <h1 class="text-2xl my-[100px]">Hello About</h1>
+  <h1 class="text-2xl my-[100px]">Hello About - Testing</h1>
 </template>
